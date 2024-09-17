7bede59214e1b578d9b54a358c8de7a271b65658
Malik Haider <malikhaidera46@gmail.com>
2024-09-17 14:54:55 +0500

    auth

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const user = require("../models/user");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const { body, validationResult } = require("express-validator");
+
+router.get("/logIn", (req, res) => {
+  res.json({ message: "Backend Login Response" });
+});
 
 router.get("/logIn", (req, res) => {
   res.json({ message: "Backend Login Response" });
